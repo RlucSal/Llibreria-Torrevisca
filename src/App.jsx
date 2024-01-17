5b91da3712dd9e2205dec6a2691922c0eb2fae43
kaiwright <kaimatthewwright@gmail.com>
2024-01-17 20:42:48 +0000

    Changes to file structure and bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import LogIn from "./Pages/LogIn";
 import Dashboard from "./Pages/Dashboard";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import NavbarEl from "./Components/NavbarEl"
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/log-in" element={<LogIn />} />
-        </Routes>
+        <NavbarEl>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/log-in" element={<LogIn />} />
+          </Routes>
+        </NavbarEl>
       </div>
       <Footer />
     </Router>
