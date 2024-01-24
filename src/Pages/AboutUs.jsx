0752e83c3b78b05f45b9651c7bd590c8d0f02be7
RlucSal <rluc.sal@gmail.com>
2024-01-24 15:46:43 +0000

    about us section and style

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AboutStyles from "../AboutStyles";
+import NavbarEl from "../Components/NavbarEl";
+import "../app.css";
 
 const AboutUs = () => {
   const [formData, setFormData] = useState({
@@ -16,75 +19,137 @@ const AboutUs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add your logic here to handle the form submission
     console.log("Form submitted:", formData);
-    // You can send the data to your server, or perform any other actions
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Contact Us</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+    <div>
+      <NavbarEl className="w-100" />
+      <div className=""> </div>
+      <h1 className="beige-text" style={AboutStyles.h1}>
+        About Us
+      </h1>
+      <div className="container" style={AboutStyles.container}>
+        <div className="row justify-content-center">
+          <div className="col-md-6" style={AboutStyles.wrapper}>
+            <div style={AboutStyles.imageWrapper}>
+              <img
+                src="/imgs/PiliErne.png"
+                alt="About Us"
+                className="img-fluid"
+                style={AboutStyles.imageWrapper.image}
+              />
+            </div>
+          </div>
+          <div className="col-md-6" style={AboutStyles.textContainer}>
+            <div className="text-container mb-4">
+              <h3>Who We Are</h3>
+              <p>
+                Torrevisca is a dream come true that was founded by Pilar
+                Brandariz and Ernestina Lucena in 2015. Two lifelong friends who
+                came from two completely different worlds and occupations, but
+                the two of us have always shared the same values, respect, and
+                the love for a job well done and the passion that accompanies us
+                in everything we do.
+              </p>
+            </div>
+            <div className="text-container">
+              <h3>Women with Strong Ideas:</h3>
+              <p>
+                The first is the love for local commerce, for commerce between
+                people, traditional and professional, that puts quality before
+                quantity. A familiar treatment where our priority is to offer a
+                friendly and honest service. At the end, we are what we offer
+                and not what we sell...
+                <br />
+                <br />
+                And the second is this revolutionary spirit that leads us to get
+                up every morning. We think it is really revolutionary to sell
+                stories, books, puzzles, board games, or wooden toys in a
+                completely digitalized era. We represent an alternative, a
+                proposal that, despite being in favor of the use of technology
+                and artificial intelligence, has a different, transformative
+                proposal that brings multiple benefits to our environment.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+
+        <h2 className="beige-text" style={AboutStyles.h2}>
+          We can't wait to hear from you
+        </h2>
+        <div style={AboutStyles.formWrapper}>
+          <form onSubmit={handleSubmit}>
+            <div style={AboutStyles.formGroup}>
+              <label htmlFor="name" style={AboutStyles.formLabel}>
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                style={AboutStyles.formControl}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" style={AboutStyles.formLabel}>
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                style={AboutStyles.formControl}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="subject" style={AboutStyles.formLabel}>
+                Subject
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="subject"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+                style={AboutStyles.formControl}
+                required
+              />
+            </div>
+            <div style={AboutStyles.formGroup}>
+              <label htmlFor="message" style={AboutStyles.formLabel}>
+                Message
+              </label>
+              <textarea
+                className="form-control"
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                rows="4"
+                style={AboutStyles.formControl}
+                required
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              style={AboutStyles.submitButton}
+            >
+              Submit
+            </button>
+          </form>
         </div>
-        <div className="mb-3">
-          <label htmlFor="subject" className="form-label">
-            Subject
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="subject"
-            name="subject"
-            value={formData.subject}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="message" className="form-label">
-            Message
-          </label>
-          <textarea
-            className="form-control"
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            rows="4"
-            required
-          ></textarea>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
